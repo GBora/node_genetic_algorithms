4715f7fa0260b031b4b960a25dd375ec228331e3
gbora <gpbora@gmail.com>
2018-05-19 20:37:20 +0300

    added readme md

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,3 @@ export { Individual } from "./individual/individual";
 export { IndividualFactory } from "./individual/individualFactory";
 export { Genotype } from "./genotype/genotype";
 export { Gene } from "./genotype/gene";
-
-
-
