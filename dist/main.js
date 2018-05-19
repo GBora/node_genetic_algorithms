f77b61648f75ab83aef664c0f0fe6000a66d65a7
gbora <gpbora@gmail.com>
2018-05-19 20:49:20 +0300

    new build

--- dist/main.js
+++ dist/main.js
@@ -1,21 +1,10 @@
-define("genome/genome", ["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var Genome = /** @class */ (function () {
-        function Genome() {
-            this.genes = [];
-        }
-        return Genome;
-    }());
-    exports.Genome = Genome;
-});
-define("individual/individual", ["require", "exports", "genome/genome"], function (require, exports, genome_1) {
+define("individual/individual", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var Individual = /** @class */ (function () {
         function Individual() {
             this.id = "0";
-            this.genome = new genome_1.Genome();
+            this.alleles = [];
             this.fitness = 0;
             this.generation = 0;
         }
@@ -24,27 +13,56 @@ define("individual/individual", ["require", "exports", "genome/genome"], functio
     exports.Individual = Individual;
     ;
 });
-define("individual/individualFactory", ["require", "exports", "individual/individual", "genome/genome"], function (require, exports, individual_1, genome_2) {
+define("genotype/gene", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Gene = /** @class */ (function () {
+        function Gene() {
+            this.id = "gene";
+            this.alleleOptions = [];
+        }
+        Object.defineProperty(Gene.prototype, "newRandomAllele", {
+            get: function () {
+                var index = Math.floor(Math.random() * this.alleleOptions.length) + 1;
+                return this.alleleOptions[index];
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Gene;
+    }());
+    exports.Gene = Gene;
+});
+define("genotype/genotype", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Genotype = /** @class */ (function () {
+        function Genotype() {
+            this.genes = [];
+        }
+        return Genotype;
+    }());
+    exports.Genotype = Genotype;
+});
+define("individual/individualFactory", ["require", "exports", "individual/individual"], function (require, exports, individual_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var IndividualFactory = /** @class */ (function () {
         function IndividualFactory() {
             this.totalIndividualsProduced = 0;
         }
-        IndividualFactory.prototype.getNewIndividual = function (genomeLength, genotype) {
+        IndividualFactory.prototype.getNewIndividual = function (genotype) {
             var individ = new individual_1.Individual();
-            // individ.genome;
             individ.fitness = 0;
             individ.generation = 0;
             individ.id = (this.totalIndividualsProduced + 1).toString();
             // Create genome
-            var genome = new genome_2.Genome();
-            genome.genes = [];
-            for (var i = 0; i < genomeLength; i++) {
-                // TODO replace this with true random genes 
-                genome.genes.push(0);
+            var alleles = [];
+            for (var i = 0; i < genotype.genes.length; i++) {
+                var gene = genotype.genes[i];
+                alleles.push(gene.newRandomAllele());
             }
-            individ.genome = genome;
+            individ.alleles = alleles;
             this.totalIndividualsProduced++;
             return individ;
         };
@@ -52,11 +70,12 @@ define("individual/individualFactory", ["require", "exports", "individual/indivi
     }());
     exports.IndividualFactory = IndividualFactory;
 });
-define("main", ["require", "exports", "individual/individual", "individual/individualFactory", "genome/genome"], function (require, exports, individual_2, individualFactory_1, genome_3) {
+define("main", ["require", "exports", "individual/individual", "individual/individualFactory", "genotype/genotype", "genotype/gene"], function (require, exports, individual_2, individualFactory_1, genotype_1, gene_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.Individual = individual_2.Individual;
     exports.IndividualFactory = individualFactory_1.IndividualFactory;
-    exports.Genome = genome_3.Genome;
+    exports.Genotype = genotype_1.Genotype;
+    exports.Gene = gene_1.Gene;
 });
 //# sourceMappingURL=main.js.map
